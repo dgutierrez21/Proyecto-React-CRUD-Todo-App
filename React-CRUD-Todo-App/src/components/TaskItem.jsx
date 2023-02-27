7f4588506d992d99b684f6c896707195f777a648
Dixon G <dxxn0101@gmail.com>
2023-02-27 09:14:56 -0600

    add state to checkbox

--- React-CRUD-Todo-App/src/components/TaskItem.jsx
+++ React-CRUD-Todo-App/src/components/TaskItem.jsx
@@ -1,15 +1,22 @@
 import styles from "./cssmodules/TaskItem.module.css";
 
 import { CheckIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export const TaskItem = ({ task }) => {
+  const [isChecked, setIsChecked] = useState(task.checked);
+
+  function handleCheckBoxChange() {
+    setIsChecked(!isChecked);
+  }
   return (
     <li className={styles.task}>
       <div className={styles["task-group"]}>
         <input
+          onChange={handleCheckBoxChange}
           className={styles.checkbox}
           type="checkbox"
-          checked={task.checked}
+          checked={isChecked}
           name={task.name}
           id={task.id}
         />
