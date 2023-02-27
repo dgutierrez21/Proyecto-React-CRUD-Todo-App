e3060398ad44eac579299b2aa2040a4e70d3094c
Dixon G <dxxn0101@gmail.com>
2023-02-27 09:06:06 -0600

    add styles to the task

--- React-CRUD-Todo-App/src/components/TaskItem.jsx
+++ React-CRUD-Todo-App/src/components/TaskItem.jsx
@@ -1,3 +1,25 @@
+import styles from "./cssmodules/TaskItem.module.css";
+
+import { CheckIcon } from "@heroicons/react/24/outline";
+
 export const TaskItem = ({ task }) => {
-  return <div>{task.name}</div>;
+  return (
+    <li className={styles.task}>
+      <div className={styles["task-group"]}>
+        <input
+          className={styles.checkbox}
+          type="checkbox"
+          checked={task.checked}
+          name={task.name}
+          id={task.id}
+        />
+        <label className={styles.label} htmlFor={task.id}>
+          {task.name}
+          <p className={styles.checkmark}>
+            <CheckIcon strokeWidth={2} width={24} height={24} />
+          </p>
+        </label>
+      </div>
+    </li>
+  );
 };
