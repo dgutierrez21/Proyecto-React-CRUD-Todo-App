f85fca43bdf79146a0ca33d67be608e51afb3e00
Dixon G <dxxn0101@gmail.com>
2023-02-28 10:14:59 -0600

    add status to activate color selection mode | add color selection mode

--- React-CRUD-Todo-App/src/components/ThemeSwitcher.jsx
+++ React-CRUD-Todo-App/src/components/ThemeSwitcher.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 
 import styles from "./cssmodules/ThemeSwitcher.module.css";
 
-import { SunIcon, SwatchIcon, MoonIcon } from "@heroicons/react/24/outline";
+import {
+  SunIcon,
+  XMarkIcon,
+  SwatchIcon,
+  MoonIcon,
+} from "@heroicons/react/24/outline";
 
 export const ThemeSwitcher = () => {
+  const [isColorPicking, setIsColorPicking] = useState(false);
+
   const [theme, setTheme] = useState("light");
 
   const changeTheme = theme === "light" ? "dark" : "light";
@@ -13,19 +20,37 @@ export const ThemeSwitcher = () => {
 
   return (
     <aside className={styles.wrapper}>
-      <div className={styles.btns}>
-        <button
-          role="switch"
-          aria-label={`Change theme to ${changeTheme} mode`}
-          onClick={handleThemeBtn}
-          className="btn"
-        >
-          {theme === "light" ? <MoonIcon /> : <SunIcon />}
-        </button>
-        <button className="btn">
-          <SwatchIcon />
-        </button>
-      </div>
+      {isColorPicking ? (
+        <>
+          <button
+            onClick={() => setIsColorPicking(false)}
+            aria-label="Close color picking mode"
+            className={`btn ${styles.close}`}
+          >
+            <XMarkIcon />
+          </button>
+          <input type="range" />
+        </>
+      ) : (
+        <div className={styles.btns}>
+          <button
+            role="switch"
+            aria-label={`Change theme to ${changeTheme} mode`}
+            onClick={handleThemeBtn}
+            className="btn"
+          >
+            {theme === "light" ? <MoonIcon /> : <SunIcon />}
+          </button>
+
+          <button
+            aria-label="Enable color picking mode"
+            onClick={() => setIsColorPicking(true)}
+            className="btn"
+          >
+            <SwatchIcon />
+          </button>
+        </div>
+      )}
     </aside>
   );
 };
