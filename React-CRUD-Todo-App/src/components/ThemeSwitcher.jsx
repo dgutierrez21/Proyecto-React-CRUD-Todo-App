d0262da72ba4033f4703c3a521b54e43661ebfe4
Dixon G <dxxn0101@gmail.com>
2023-02-28 09:54:09 -0600

    add status to change icon if theme changes

--- React-CRUD-Todo-App/src/components/ThemeSwitcher.jsx
+++ React-CRUD-Todo-App/src/components/ThemeSwitcher.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 import styles from "./cssmodules/ThemeSwitcher.module.css";
 
-import { SwatchIcon, MoonIcon } from "@heroicons/react/24/outline";
+import { SunIcon, SwatchIcon, MoonIcon } from "@heroicons/react/24/outline";
 
 export const ThemeSwitcher = () => {
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = theme === "light" ? "dark" : "light";
+
+  const handleThemeBtn = () => setTheme(changeTheme);
+
   return (
     <aside className={styles.wrapper}>
       <div className={styles.btns}>
-        <button className="btn">
-          <MoonIcon />
+        <button
+          role="switch"
+          aria-label={`Change theme to ${changeTheme} mode`}
+          onClick={handleThemeBtn}
+          className="btn"
+        >
+          {theme === "light" ? <MoonIcon /> : <SunIcon />}
         </button>
         <button className="btn">
           <SwatchIcon />
